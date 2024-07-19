45f395393b0116cdc12f94ef61fb4b2f9341134e
Eben <bonuzx1@gmail.com>
2024-07-19 22:28:01 +0000

    added templates and components

--- base/views.py
+++ base/views.py
@@ -82,6 +82,15 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user':user, 'rooms':rooms, 'room_messages':room_messages, 'topics':topics}
+    return render(request, 'base/profile.html', context)
+
+
 @login_required(login_url='login')
 def createRoom(request):
     formroom = RoomForm()
@@ -89,7 +98,9 @@ def createRoom(request):
     if request.method == 'POST':
         formroom = RoomForm(request.POST)
         if formroom.is_valid():
-            formroom.save()
+            room = formroom.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
     context = {'roomfields': formroom}
     return render(request, 'base/room_form.html', context)
