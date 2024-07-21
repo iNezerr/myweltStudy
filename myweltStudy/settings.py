10f7829f47c62390ddba8f481cb552caa5eb2704
Eben <bonuzx1@gmail.com>
2024-07-21 14:10:56 +0000

    added staticfiles

--- myweltStudy/settings.py
+++ myweltStudy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-d2cahs7b&xci8-eu=&tkk&f^8hd-5%&2-5k)@b)6=8$g8okl1x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ebendidthis.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 STATICFILES_DIRS = [
